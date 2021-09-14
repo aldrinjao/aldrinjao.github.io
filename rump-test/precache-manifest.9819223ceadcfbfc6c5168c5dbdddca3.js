deb8b238968658e02b0e1c73fe499aa2bca0c05f
aldrinjao <aldrinhao@gmail.com>
2021-09-14 12:38:42 +0800

    rump test

---
+++ rump-test/precache-manifest.9819223ceadcfbfc6c5168c5dbdddca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5bdc105751048dcf6a514b248e9019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78683d1961c5ed6cf708",
+    "url": "/static/css/main.65a02a18.chunk.css"
+  },
+  {
+    "revision": "fba213e367dd260cc543",
+    "url": "/static/js/2.c3151dd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3151dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78683d1961c5ed6cf708",
+    "url": "/static/js/main.875deb51.chunk.js"
+  },
+  {
+    "revision": "70c44dad4d78b18fa89d",
+    "url": "/static/js/runtime-main.ae69690c.js"
+  }
+]);
